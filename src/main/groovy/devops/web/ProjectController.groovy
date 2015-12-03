1a069473af9f70ac29fbd2ed278fa3627aea52ab
Sion Williams <sion5@hotmail.co.uk>
2015-12-03 21:34:36 +0000

    fix(codenarc): doesn't work with Travis CI

--- src/main/groovy/devops/web/ProjectController.groovy
+++ src/main/groovy/devops/web/ProjectController.groovy
@@ -7,7 +7,6 @@ import devops.services.StashService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -33,12 +32,7 @@ class ProjectController {
     @RequestMapping(method = RequestMethod.POST)
     String projectSubmit(@ModelAttribute Project project,
                          @ModelAttribute Features features,
-                         BindingResult bindingResult,
                          Model model) {
-
-        if (bindingResult.hasErrors()) {
-            return "project"
-        }
 
         if (features.jira) {
             def location = jiraService.createProject(project)
